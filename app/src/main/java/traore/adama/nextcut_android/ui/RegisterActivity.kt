155830072a1bd5b03a1761dce6c95696e41384c1
Adama Traoré <atraore@rayonnance.fr>
2019-06-03 16:52:00 +0200

    event txvLogin/txvRegister

--- app/src/main/java/traore/adama/nextcut_android/ui/RegisterActivity.kt
+++ app/src/main/java/traore/adama/nextcut_android/ui/RegisterActivity.kt
@@ -2,36 +2,36 @@ package traore.adama.nextcut_android.ui
 
 import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import traore.adama.nextcut_android.R
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
 
-    val Tag: String = LoginActivity::class.java.simpleName;
+    val Tag: String = RegisterActivity::class.java.simpleName;
 
     /**
      * Launch method -> start activity
      */
     companion object {
         fun launch(activity: Activity){
-            activity.startActivity(Intent(activity, LoginActivity::class.java))
+            activity.startActivity(Intent(activity, RegisterActivity::class.java))
             activity.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
     }
 
     override fun onStart() {
         super.onStart()
 
-        txvRegisterLink.setOnClickListener {
-            RegisterActivity.launch(this)
+        txvLoginLink.setOnClickListener {
+            LoginActivity.launch(this)
         }
     }
 }
