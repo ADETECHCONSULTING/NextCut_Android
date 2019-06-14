6d258231573223da6be04d4f60e561e0fc1b10dd
Adama <adama75x2x@gmail.com>
2019-06-14 22:21:52 +0200

    explorer binding loading

--- app/src/main/java/traore/adama/nextcut_android/ui/fragment/ExploreFragment.kt
+++ app/src/main/java/traore/adama/nextcut_android/ui/fragment/ExploreFragment.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_explore.*
+import com.google.android.material.snackbar.Snackbar
 import traore.adama.nextcut_android.R
 import traore.adama.nextcut_android.databinding.FragmentExploreBinding
 import traore.adama.nextcut_android.ui.adapter.ExploreAdapter
@@ -20,18 +22,28 @@ class ExploreFragment : Fragment() {
 
     private lateinit var binding: FragmentExploreBinding
     private lateinit var viewModel: ExploreViewModel
+    private var errorSnackbar: Snackbar? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_explore, container, false)
         val view = binding.root
 
-        //rcvHaircuts.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        binding.rcvHaircuts.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
 
         viewModel = ViewModelProviders.of(this).get(ExploreViewModel::class.java)
+        viewModel.errorMessage.observe(this, Observer {errorMessage ->
+            if (errorMessage != null)
+                showError(errorMessage)
+            else
+                hideError()
+
+        })
+
 
         //Set values to layout
         binding.viewModel = viewModel
+
 
         return view
     }
@@ -42,5 +54,15 @@ class ExploreFragment : Fragment() {
 
     }
 
+
+    private fun showError(@StringRes errorMessage:Int){
+        errorSnackbar = Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_INDEFINITE)
+        errorSnackbar?.setAction(R.string.retry, viewModel.errorClickListener)
+        errorSnackbar?.show()
+    }
+
+    private fun hideError(){
+        errorSnackbar?.dismiss()
+    }
 
 }
--- app/src/main/java/traore/adama/nextcut_android/viewmodel/ExploreViewModel.kt
+++ app/src/main/java/traore/adama/nextcut_android/viewmodel/ExploreViewModel.kt
@@ -1,10 +1,12 @@
 package traore.adama.nextcut_android.viewmodel
 
 import android.view.View
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import traore.adama.nextcut_android.R
 import traore.adama.nextcut_android.services.network.MyWebApi
 import javax.inject.Inject
 
@@ -15,6 +17,8 @@ class ExploreViewModel:BaseViewModel(){
 
     private lateinit var subscription: Disposable
     val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
+    val errorMessage: MutableLiveData<Int> = MutableLiveData()
+    val errorClickListener = View.OnClickListener { loadHaircuts() }
 
     init {
         loadHaircuts()
@@ -46,7 +50,7 @@ class ExploreViewModel:BaseViewModel(){
     }
 
     private fun onRetrieveHaircutListError(){
-
+        errorMessage.value = R.string.haircuts_fetch_error
     }
 
 
@@ -55,4 +59,6 @@ class ExploreViewModel:BaseViewModel(){
         if(!subscription.isDisposed)
             subscription.dispose()
     }
+
+
 }
